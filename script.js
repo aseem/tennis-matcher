e3815e56a575dbbb3c6716d7b7fc6cb29a6ceee7
Aseem Kohli <aseemko@gmail.com>
2022-07-05 15:14:32 -0400

    Add player scores

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ const nav_round1 = document.getElementById("nav-round1");
 const nav_round2 = document.getElementById("nav-round2");
 const nav_round3 = document.getElementById("nav-round3");
 
+const player_table = document.getElementById("player-table");
+
+var player_scores = [
+  { player: "Aseem", score: 0 },
+  { player: "Diana", score: 0 },
+  { player: "Chris", score: 0 },
+  { player: "Romie", score: 0 },
+  { player: "Pamela", score: 0 },
+  { player: "Rachel", score: 0 },
+  { player: "Deb", score: 0 },
+  { player: "Stu", score: 0 },
+  { player: "Matt", score: 0 },
+  { player: "John", score: 0 },
+  { player: "Ron", score: 0 },
+  { player: "Elisa", score: 0 },
+  { player: "Laura", score: 0 },
+  { player: "MP", score: 0 },
+  { player: "Nate", score: 0 },
+];
+
 // Toggle nav
 toggle.addEventListener("click", () =>
   document.body.classList.toggle("show-nav")
@@ -26,6 +46,29 @@ window.addEventListener("click", (e) =>
 
 // Toggle players content
 nav_players.addEventListener("click", () => {
+  player_table.innerHTML = "";
+
+  var header_row = document.createElement("tr");
+  var h1 = document.createElement("th");
+  var h2 = document.createElement("th");
+  h1.textContent = "Player";
+  h2.textContent = "Score";
+  header_row.appendChild(h1);
+  header_row.appendChild(h2);
+  player_table.appendChild(header_row);
+
+  for (var i = 0; i < player_scores.length; i++) {
+    var row = document.createElement("tr");
+    var cell1 = document.createElement("td");
+    cell1.textContent = player_scores[i]["player"];
+    var cell2 = document.createElement("td");
+    cell2.textContent = player_scores[i]["score"];
+
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    player_table.appendChild(row);
+  }
+
   document.getElementById("main-container").innerHTML =
     document.getElementById("players-container").innerHTML;
   document.body.classList.toggle("show-nav");
